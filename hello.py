548928fde1ed7194ec3f57a3fee003e7b24f1540
EndDotExe <hughiemck@icloud.com>
2025-02-09 01:36:36 +0000

    First video complete :]

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-# ask the user for their name
-name = input("What's your name? ").strip().title()
+def main():
+    name = input("What's your name? ")
+    hello(name)
 
-#split user's name into first and last name
-first, last = name.split(" ")
 
-# say hello to the user
-print (f"Hello, {name}")
+def hello(to = "world"):
+    print("Hello,", to)
+
+
+main()
