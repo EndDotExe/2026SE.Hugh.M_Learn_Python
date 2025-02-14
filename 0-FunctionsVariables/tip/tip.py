3b492721d7aeb9f968842f18c2cde9bdaa1e9a52
EndDotExe <hughiemck@icloud.com>
2025-02-14 01:34:06 +0000

    finished problem 0.5 :>

--- 0-FunctionsVariables/tip/tip.py
+++ 0-FunctionsVariables/tip/tip.py
@@ -2,14 +2,17 @@ def main():
 	dollars = dollars_to_float(input("How much was the meal? "))
 	percent = percent_to_float(input("What percentage would you like to tip? "))
 	tip = dollars * percent
-    print(f"Leave ${tip:.2f}")
+	print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
-	# TODO
-
+	# convert the dollars into a float
+	result = d.replace ('$', '')
+	return float (result)
 
 def percent_to_float(p):
-	# TODO
+	# convert the tip percentage into a float
+	tipp = p.replace('%', '')
 	
+	return float (tipp) / 100
 main()
