2d14ed41e3c830f3efb08c9504fc48d9fffda71a
EndDotExe <hughiemck@icloud.com>
2025-02-20 23:02:17 +0000

    problem 1-3 done

--- 1-Conditionals/extensions/extensions.py
+++ 1-Conditionals/extensions/extensions.py
@@ -1,9 +1,16 @@
 itsgifnotjif = input(" ")
 
-match itsgifnotjif:
+name, filetype = itsgifnotjif.split(".")
+filetype = filetype.lower()
+
+match filetype:
     case "png"|"gif"|"jpeg"|"jpg":
-        print(f"image/{itsgifnotjif}")
+        print(f"image/{filetype}")
     case "mp4"|"mov":
-        print(f"video/{itsgifnotjif}")
+        print(f"video/{filetype}")
     case "mp3"|"wav":
-        print(f"audio/{itsgifnotjif}")
+        print(f"audio/{filetype}")
+    case "pdf"|"txt"|"zip":
+        print (f"application/{filetype}")
+    case _:
+        print("application/octet-stream")
