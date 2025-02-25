a585af4c668e3e39153f340d81c692c6beac9c00
EndDotExe <hughiemck@icloud.com>
2025-02-25 00:54:31 +0000

    camel.py started,,, how,,,,

---
+++ 2-Loops/camel/camel.py
@@ -0,0 +1,6 @@
+meow = input("Variable Name: ")
+
+for letter in meow:
+    if letter.isupper():
+        print("in the stripped club, straight up jorkin it, and by 'it', heh,, lets just say... my penits")
+
