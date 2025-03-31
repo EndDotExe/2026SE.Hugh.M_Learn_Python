703866d9ed0ce1d89aa3c51c73ca8022905e3bc2
EndDotExe <hughiemck@icloud.com>
2025-03-31 00:56:14 +0000

    mondaychallenge murdered my family :[

---
+++ mondaychallenge.py
@@ -0,0 +1,33 @@
+# create two lists
+
+listone = ['1', '2', '3', '4', '5']
+listtwo = ['9', '8', '7', '6', '5']
+lists = [listone, listtwo]
+# create two lists with an ID so line items can be paired
+
+#listids = [
+#    {'listone': '1', 'listtwo': '9'},
+#    {'listone': '2', 'listtwo': '8'},
+#    {'listone': '3', 'listtwo': '7'},
+#    {'listone': '4', 'listtwo': '6'},
+#    {'listone': '5', 'listtwo': '5'}
+#]
+
+# print the two lists as a table
+
+print(lists)
+
+
+# add a new item & sort the lists
+
+
+
+# create a text document with the lists
+
+
+
+# read from the lists
+
+
+
+# edit lines and write to lists
