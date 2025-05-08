594dc897789cbaa4f06354ef01ee3287ca718b2b
EndDotExe <hughiemck@icloud.com>
2025-05-08 00:34:49 +0000

    OOP video - 1:30:27

--- student.py
+++ student.py
@@ -1,17 +1,33 @@
 class Student:
-    ...
+    def __init__(self, name, house, ):
+        if not name:
+            raise ValueError("Missing Name")
+        self.name = name 
+        self.house = house
 
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"\
+    
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
 
 def main(): 
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    print(student)
 
 
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house, )
 
 
 if __name__ == "__main__":
