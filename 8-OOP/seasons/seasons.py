df390e2e5187338afeab797075218080d670deec
EndDotExe <hughiemck@icloud.com>
2025-05-15 23:23:29 +0000

    seasons.py complete RAAHHHHHHHHH

--- 8-OOP/seasons/seasons.py
+++ 8-OOP/seasons/seasons.py
@@ -2,9 +2,16 @@ from datetime import date
 
 def main():
     #prompt user for their DOB:
-    dateofbirth = date(input("Date of Birth: (YYYY•MM•DD)"))
-    timealive = int(date.today - dateofbirth) 
+    dateofbirth = date.fromisoformat(input("Date of Birth (YYYY-MM-DD): "))
+    currentdate = date.today()
+    timealive = (currentdate - dateofbirth)
+    minutes = 1440 * (timealive)
+    youexist = str(minutes).split(' ')[0]
+    
+    print(f"Today's date is {currentdate}")
+    print(f"You've been alive for roughly {youexist} minutes!")
 
 
 if __name__ == "__main__":
     main()
+
